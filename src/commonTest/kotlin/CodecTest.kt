e8c3c6afd07953c3c3d2cf028b4b1c491925b3a9
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2024-03-02 14:54:16 +0100

    test: test Codec.readHeader

--- src/commonTest/kotlin/CodecTest.kt
+++ src/commonTest/kotlin/CodecTest.kt
@@ -1,14 +1,23 @@
 package cz.smarteon.loxone
 
-import cz.smarteon.loxone.Codec.loxJson
+import cz.smarteon.loxone.message.MessageHeader
+import cz.smarteon.loxone.message.MessageKind
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import kotlinx.serialization.builtins.ByteArraySerializer
 
+@OptIn(ExperimentalStdlibApi::class)
 class CodecTest : StringSpec({
 
     "should decode hex" {
-        println(loxJson.encodeToString(ByteArraySerializer(), byteArrayOf(50, 53)))
         Codec.hexToBytes("3235") shouldBe byteArrayOf(50, 53)
+    }
+
+    "should encode hex" {
+        Codec.bytesToHex(byteArrayOf(50, 53)) shouldBe "3235"
+    }
+
+    "should read header" {
+        val header = Codec.readHeader(Codec.hexToBytes("03000000ab000000"))
+        header shouldBe MessageHeader(MessageKind.TEXT, false, 43776)
     }
 })
